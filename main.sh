85eed7192271e41dcc07307d1f7b915eadabaf14
hardik <hardikbafna56@gmail.com>
2025-11-13 17:40:37 +0530

    db part done encrypt need to check

--- main.sh
+++ main.sh
@@ -1,52 +1,63 @@
-#NEED TO HANDLE EDGE CASES AND ADD MORE COMMENTS
-
-
-#We execute the functions script to use its functions here
 #!/bin/bash
 
 source ./functions.sh
 
-run(){
-master_exists=0
-entry=0
-check_master
-if [ "$master_exists" -eq 1 ]; then
+run() {
+
+    # Step 1: Check if master password exists
+    check_master
+
+    # If no master password, create one
+    if [ "$master_exists" -eq 0 ]; then
+        create_master
+    fi
+
+    # Step 2: Authenticate user
     vault_entry
-        if [ "$entry" -eq 1 ] ; then
-                main_menu
-                    if [ "$choice" == "View Passwords"]; then
-                        view_pass
-                        main_menu
-                    elif  [ "$choice" == "Manage Passwords" ]; then
-                        manage_pass_menu
-                            if [ "$man_choice" -eq 1 ]; then
-                                add_pass_menu
-                                    if [ "$add_choice" -eq 1 ]; then
-                                        add_pass
-                                    elif [ "$add_choice" -eq 2 ]; then
-                                        auto_gen_pass
-                                    fi
-                            elif [ "$man_choice" -eq 2 ]; then
-                                delete_pass
-                            elif [ "$man_choice" -eq 3 ]; then
-                                edit_pass
-                            elif [ "$man_choice" -eq 4 ]; then
-                                main_menu
-                            fi
-                    elif [ "$choice" == "Change Master Password" ]; then
-                        change_master
-                        run
-                    elif [ "$choice" == "Exit" ]; then
-                        return 0
-                    fi
-        else
-            echo "Exiting..."
-            return 0
-        fi
-else
-    create_master
-    run
-fi
+    if [ "$entry" -ne 1 ]; then
+        echo "Exiting..."
+        exit 0
+    fi
+
+    # Step 3: Main loop
+    while true; do
+
+        main_menu
+
+        case "$choice" in
+            1)
+                view_pass
+                ;;
+            2)
+                manage_pass_menu
+                case "$man_choice" in
+                    1)
+                        add_pass_menu
+                        case "$add_choice" in
+                            1) add_pass ;;
+                            2) auto_gen_pass ;;
+                            3) ;; # back
+                        esac
+                        ;;
+                    2) delete_pass ;;
+                    3) edit_pass ;;
+                    4) ;; # back
+                esac
+                ;;
+            3)
+                change_master
+                ;;
+            4)
+                echo "Goodbye!"
+                exit 0
+                ;;
+            *)
+                echo "Invalid choice."
+                ;;
+        esac
+
+    done
 }
 
 run
+
